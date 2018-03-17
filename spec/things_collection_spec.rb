faafdd63f898ddbafff71558d9247e09b878b683
iliyasold <iliyasold@gmail.com>
2018-03-18 00:20:45 +0300

    Extended #for_temperature test

--- spec/things_collection_spec.rb
+++ spec/things_collection_spec.rb
@@ -9,5 +9,6 @@ describe 'ThingsCollection' do
 
   it '#for_temperature' do
     expect(collection.for_temperature(10).size).to eq 1
+    expect(collection.for_temperature(10).map(&:to_s)).to match_array(['Кепка (Головной убор) 0..10'])
   end
 end
